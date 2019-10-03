120a4e8e986ee40cfbb4cc360004647e05d210c0
Joshua Jonas <mr.joshuajonas@gmail.com>
2019-10-03 10:27:08 -0700

    post and routing complete

--- app/server.js
+++ app/server.js
@@ -1,5 +1,16 @@
 var express =  require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+var surveyAnswers = [{
+    q1:2,
+    q2:4
+}];
+
+var friendsList = [];
 
 app.get('/', function (req,res){
     res.send('this is the home page')
@@ -10,14 +21,20 @@ app.get('/survey', function (req,res){
 });
 
 app.get('/api/survey', function (req,res){
-    res.send('this is the survey data')
+    res.json(surveyAnswers)
 });
+
+app.get('/api/friends', function (req,res){
+    res.json(friendsList)
+});
+
+app.post('/api/survey', function (req, res){
+    var newFriend = req.body;
+    surveyAnswers.push(newFriend);
+    res.json(newFriend);
+})
 
 app.listen(3000, function (){
     console.log('server running on port 3000');
     
-})
-
-app.post('/api/survey', function (req, res){
-    res.send('this is api for surveys post route')
 })
