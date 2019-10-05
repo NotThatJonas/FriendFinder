8ee9f6894aa66387195da478669bc612201f67c8
Joshua Jonas <mr.joshuajonas@gmail.com>
2019-10-04 18:02:29 -0700

    changes, function for scoring

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,32 +1,71 @@
 var express =  require('express');
 var router = express.Router();
-var path = require('path');
 
-// createing arrays to push object to
-var surveyAnswers = [{
-    q1:2,
-    q2:4
-}];
-
-var friendsList = [];
-
+// Link in Friends Data
+var friendsData = require('../data/friends.js');
 
 //data routes for api
 router.get('/survey', function (req,res){
-    res.json(surveyAnswers)
+    res.json(friendsData)
 });
 
 //data routes for api
 router.get('/friends', function (req,res){
-    res.json(friendsList)
+    res.json(friendsData)
 });
 
+
 //creates new friend based on who has filled out form
-router.post('/survey', function (req, res){
-    var newFriend = req.body;
-    surveyAnswers.push(newFriend);
-    res.json(newFriend);
+router.post('/friends', function (req, res){
+    var newFriend = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: []
+      };
+      
+    
+    console.log('friends', friendsData);
+    var bestMatch = {
+        name: "",
+        photo: "",
+        friendDifference: Infinity
+    }
+    var totalDifference;
+
+      for (let i = 0; i < friendsData.length - 1; i++) {
+        var possibleMatches = friendsData;
+        totalDifference = 0;
+        var currentFriend = friendsData[i];
+        var absoluteDifference = [];
+        for (let j = 0; j < currentFriend.scores.length; j++) {
+        
+           var currentUser = friendsData[friendsData.length - 1];
+           var answerDifference = Math.abs(currentFriend.scores[j] - currentUser.scores[j]);
+           absoluteDifference.push(answerDifference)
+            
+        }
+        
+        var reducedDifference = absoluteDifference.reduce(reduceFunction)
+        console.log('red', reducedDifference);
+        if (reducedDifference < bestMatch.friendDifference){
+            bestMatch.name = friendsData[i].name;
+            bestMatch.photo = friendsData[i].photo;
+            // bestMatch.friendDifference = absoluteDifference[i];
+        }
+        
+       
+          
+      }
+      console.log('tet', bestMatch);
+      
+
+      friendsData.push(newFriend);
+  
 })
+
+function reduceFunction(total, num) {
+    return total + num;
+  }
 
 
 module.exports = router;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,13 +4,13 @@ var path = require('path');
 
 //html routes, will send back home page
 router.get('/', function (req,res){
-    res.sendFile(path.join(__dirname, '../home.html'))
+    res.sendFile(path.join(__dirname, '../public/home.html'))
 });
 
 
 //add survey page
 router.get('/survey', function (req,res){
-    res.sendFile(path.join(__dirname, '../survey.html'))
+    res.sendFile(path.join(__dirname, '../public/survey.html'))
 });
 
 module.exports = router;
